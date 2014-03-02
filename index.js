1d122ef97eb5e89e6f35732227d70fb22073f288
badog1011 <den2092@gmail.com>
2014-03-02 23:49:48 +0800

    P56 把目錄底下的子檔案打開
    判斷是否為目錄stats[Number(data)].isDirectory()
    且另外建立一個全域變數儲存檔案的名稱避免再次執行fs.stat

--- index.js
+++ index.js
@@ -17,21 +17,23 @@ fs.readdir(process.cwd(), function (err, files) {
 	/**
 	*讓每個元素接執行此函數
 	**/
+	var stats = [];
 	function file(i) {
 		var filename = files[i];//抓取檔案名稱
-
+		
 		fs.stat(__dirname + '/' + filename, function (err, stat) {
+			stats[i] = stat;//為了避免再次執行fs.stat，將file函式中Stat對象保存下來
 			if (stat.isDirectory()) {
 				console.log('	'+i+'	\033[36m' + filename + '/\033[39m');//如果是目錄 文本用綠色顯示
 			} else {
-				console.log('	'+i+'	\033[90m' + filename + '\033[39m');//一般檔案則用 灰色顯示
+				console.log('	'+i+'	\033[211m' + filename + '\033[39m');//一般檔案則用 灰色顯示
 			}
 
 			
 			if (++i == files.length) {
 				read();
 			} else {
-				file(i);//執行函式 直到顯示所有檔案才結束
+				file(i);//執行函式 直到顯示所有檔案才結束 //採Recursive方式
 			}
 		});
 	}
@@ -39,7 +41,7 @@ fs.readdir(process.cwd(), function (err, files) {
 	//read user input when files are shown
 	function read() {
 		console.log('');
-		process.stdout.write('	\033[33mEnter your choice: \33[39m');
+		process.stdout.write('	\033[33mEnter your choice: \33[39m');//利用console.log不用換行輸入
 		process.stdin.resume();
 		process.stdin.setEncoding('utf8'); //設置stream編碼為utf8即可支援特殊符號
 
@@ -48,11 +50,19 @@ fs.readdir(process.cwd(), function (err, files) {
 
 	//called with the option supplied by the user
 	function option(data) {
-		var filename = files[Number(data)];
-		if (!files[Number(data)]) {
-			stdout.write('	\033[31mEnter your choice: \33[39m');
+		var filename = files[Number(data)];//抓取files[i]資料
+		if (stats[Number(data)].isDirectory()) {
+			fs.readdir(__dirname + '/' + filename, function (err, files) {
+				console.log('');
+				console.log('	(' + file.length + 'files)');
+				files.forEach(function (file) {
+					console.log('	-	' + file);
+				});
+				console.log('');
+			});
+			// stdout.write('	\033[31mEnter your choice: \33[39m');
 		} else {
-			stdin.pause();
+			// stdin.pause();
 			fs.readFile(__dirname + '/' + filename, 'utf8', function (err, data) {//事先指定編碼，(utf8)這樣數據就可以得到相應的字串符號了
 				console.log('');
 				console.log('\033[90m' + data.replace(/(.*)/g, '	$1') + '\033[39m');//使用正規表達式來添加輔助縮寫後將文件內容進行輸出
