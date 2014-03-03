380fa34d8009a6d3fdd3161b87b6381bc8d860f6
badog1011 <den2092@gmail.com>
2014-03-03 19:18:04 +0800

    P85 setTimeout
    在調用end前可多次調用write來多次發送訊息，當end Node會結束響應不再允許寫入。

--- HTTP/index.js
+++ HTTP/index.js
@@ -1,4 +1,9 @@
 require('http').createServer(function (req, res) {
 	res.writeHead(200, { 'Content-Type': 'text/html' });//Brownser不知道服務器發送過來的內容是什麼
-	res.end('Hello <b>World</b>!');
+	res.end('Hello');
+
+	setTimeout(function () {
+
+		res.end('World');
+	}, 500);
 }).listen(3000);
