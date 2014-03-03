2c86171bfb41c1fed8c51bff1288d7d6cbba52c2
丁小豪 <den2092@gmail.com>
2014-03-03 22:53:22 +0800

    P98

--- Twitter Web/client.js
+++ Twitter Web/client.js
@@ -1,22 +1,26 @@
 var  http = require('http')
-	,qs = request('querystring')
+	,qs   = require('querystring')
 
 
 function send (theName) {
-	http
-}
-require('http').request({
+	http.request({
 	 host:   '127.0.0.1'
 	,port:  '3000'
 	,url:    '/'
-	,method: 'GET'
-}, function (res) {
-	var body = '';
-	res.setEncoding('utf8');
-	res.on('data', function (chunk) {
-		body += chunk;
-	});
-	res.on('end', function () {
-		console.log('\n We got: \033[96m' + body + '\033[39m\n');
-	});
-}).end();
+	,method: 'POST'
+	}, function (res) {
+		res.setEncoding('utf8');
+		res.on('end', function () {
+			console.log('\n 	\033[90m 	request complete!\033[39m\n');
+			process.stdout.write('\n 	your name: ');
+		});
+}).end(qs.stringify({ name: theName }));
+}
+
+process.stdout.write('\nYour name: ');
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+process.stdin.on('data', function (name) {
+	send(name.replace('\n', ''));
+
+});
