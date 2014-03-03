7bdf0e1306a114b34d6ffd64014c19ffada28eae
badog1011 <den2092@gmail.com>
2014-03-03 19:22:25 +0800

    P86 ReadStream文件系統API實現

--- HTTP/index.js
+++ HTTP/index.js
@@ -1,9 +1,12 @@
 require('http').createServer(function (req, res) {
-	res.writeHead(200, { 'Content-Type': 'text/html' });//Brownser不知道服務器發送過來的內容是什麼
-	res.end('Hello');
+	res.writeHead(200, { 'Content-Type': 'image/png' });//Brownser不知道服務器發送過來的內容是什麼
+	var stream = require('fs').createReadStream('image.png');
+	stream.on('data', function (data) {
+		res.write(data);
+	});
+	stream.on('end', function (){
+		res.end();
+	});
 
-	setTimeout(function () {
-
-		res.end('World');
-	}, 500);
+	
 }).listen(3000);
