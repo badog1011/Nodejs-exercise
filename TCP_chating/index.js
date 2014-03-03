87e11961078a3caeb437a31dad2be079bd576a27
badog1011 <den2092@gmail.com>
2014-03-03 17:52:54 +0800

    P74 nickname之判別
    若重覆則要求重新輸入新的nickname

--- TCP_chating/index.js
+++ TCP_chating/index.js
@@ -8,20 +8,38 @@ var net = require('net')
 *創建服務器
 **/
 
+
+
 /*追蹤連接數*/
-var count = 0;
+var count = 0
+   ,users = {}
 
 var server = net.createServer(function (conn) {
+	var nickname;
 	conn.setEncoding('utf8');//設定編碼方式
+	
 	conn.write(
-		  '\n > welcome to node chat!'
-		+ '\n > ' + count + ' other people are connected at this time.' 
+		  '\n > welcome to \033[92mnode-chat\033[39m!'
+		+ '\n > ' + count + ' other people are connected at this time.'
 		+ '\n > please write your name and press enter: '
 		);
 	count++;
-
+	
 	conn.on('data', function (data) {
-		console.log(data);
+		data = data.replace('\r\n', '');
+		if (!nickname) {
+			if (users[data]) {
+				conn.write('\n\033[93m> nickname already in use. try again:\033[39m ');
+				return;
+			} else {
+				nickname = data;
+				users[nickname] = conn;
+
+				for (var i in users) {
+					users[i].write('\033[90m > ' + nickname + ' joined the room\033[39m\n');
+				}
+			}
+		}
 	});
 	conn.on('close', function () {
 		count--;
