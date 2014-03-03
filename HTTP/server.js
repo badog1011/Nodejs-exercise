13cbce06b4f51530ec35be4034213f3e6eedbb88
badog1011 <den2092@gmail.com>
2014-03-03 20:36:03 +0800

    P94 整合
    當submit名字時透過querystring處理顯現處來

--- HTTP/server.js
+++ HTTP/server.js
@@ -1,3 +1,4 @@
+var qs = require('querystring');
 require('http').createServer(function (req, res) {
 	if ('/' == req.url) {
 		res.writeHead(200, { 'Content-Type': 'text/html' });
@@ -18,8 +19,7 @@ require('http').createServer(function (req, res) {
 			});
 			req.on('end', function () {
 				res.writeHead(200, {'Content-Type': 'text/html'});
-				res.end('<p>Content-Type: ' + req.headers['content-type'] + '<p>'
-					+ '<p>Data:</p><pre>' + body + '</pre>');
-			});			
+				res.end('<p>Your name is:<b>' + qs.parse(body).name + '</b></p>');
+			});
 		}
 }).listen(3000);
