3831c2bf999e5aa9870f548c110a11b932b4804b
badog1011 <den2092@gmail.com>
2014-03-03 20:20:11 +0800

    P92 數據
    並顯示Head info.

--- HTTP/server.js
+++ HTTP/server.js
@@ -2,7 +2,7 @@ require('http').createServer(function (req, res) {
 	if ('/' == req.url) {
 		res.writeHead(200, { 'Content-Type': 'text/html' });
 		res.end([
-			 '<form method="POST" action="/url">'
+			 '<form method="POST" action="/url">'//瀏覽器會透過<form>標籤中action屬性指定的HTTP方式將表單數據發送過去。
 			,'<h1>My form<h1>'
 			,'<fieldset>'
 			,'<label>Personal information</label>'
@@ -11,8 +11,15 @@ require('http').createServer(function (req, res) {
 			,'<p><button>Submit</button></p>'
 			,'</form>'
 			].join(''));
-		} else if ('/url' == req.url) {
-			res.writeHead(200, {'Content-Type': 'text/html'});
-			res.end('You set a <em>' + req.method + '</em> request');
+		} else if ('/url' == req.url && 'POST'== req.method) {
+			var body = '';
+			req.on('data', function (chunk) {
+				body += chunk;
+			});
+			req.on('end', function () {
+				res.writeHead(200, {'Content-Type': 'text/html'});
+				res.end('<p>Content-Type: ' + req.headers['content-type'] + '<p>'
+					+ '<p>Data:</p><pre>' + body + '</pre>');
+			});			
 		}
 }).listen(3000);
