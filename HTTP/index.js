ce5a8b1e1d36af25e79e58bbe3d19fe7f1796622
badog1011 <den2092@gmail.com>
2014-03-03 19:06:17 +0800

    P84 告訴對方信息內容為text/plain

--- HTTP/index.js
+++ HTTP/index.js
@@ -1,4 +1,4 @@
 require('http').createServer(function (req, res) {
-	res.writeHead(200);//, { 'Content-Type': 'text/html' }
+	res.writeHead(200, { 'Content-Type': 'text/html' });//Brownser不知道服務器發送過來的內容是什麼
 	res.end('Hello <b>World</b>!');
 }).listen(3000);
